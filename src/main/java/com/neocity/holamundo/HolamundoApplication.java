8ff36954c580d8d57f0fae4f03c01477d0d3c0c6
w17714mR <william.rehe@gmail.com>
2019-12-23 09:20:38 -0300

    TESTING

---
+++ src/main/java/com/neocity/holamundo/HolamundoApplication.java
@@ -0,0 +1,16 @@
+package com.neocity.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoApplication.class, args);
+
+		System.out.println("Hola Mundo");
+
+	}
+
+}
